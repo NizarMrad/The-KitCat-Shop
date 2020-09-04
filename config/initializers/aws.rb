4f635ba69771584cfb089378a7783baacf4d281b
NizarMrad <nizar.mrad.pro@gmail.com>
2020-09-04 02:05:27 +0200

    s3 ça marche pas

--- config/initializers/aws.rb
+++
@@ -1,6 +0,0 @@
-Aws.config.update({
-  region: 'eu-west-3',
-  credentials: Aws::Credentials.new(ENV['BUCKETEER_AWS_ACCESS_KEY_ID'], ENV['BUCKETEER_AWS_SECRET_ACCESS_KEY']),
-})
-
-S3_BUCKET = Aws::S3::Resource.new.bucket(ENV['BUCKETEER_BUCKET_NAME'])
